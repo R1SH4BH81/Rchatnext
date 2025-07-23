b57a83c5800c46d4efb68aed8e4b3723dfb70bc8
RISHABH MISHRA <92362688+R1SH4BH81@users.noreply.github.com>
2025-07-23 10:39:53 +0530

    Update userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,15 +2,19 @@ import User from "../models/User.js";
 
 export const upsertUser = async (req, res) => {
   const { uid, name, email } = req.body;
+  console.log("🟢 Received upsertUser request:", { uid, name, email });
+
   try {
     const user = await User.findOneAndUpdate(
       { uid },
       { name, email },
       { upsert: true, new: true }
     );
+    console.log("✅ Mongo response:", user);
     res.json(user);
   } catch (err) {
     console.error("❌ Upsert user failed:", err);
     res.status(500).json({ message: "Internal server error" });
   }
 };
+
